5780a6cae95848349f75ec127f8038fd333a90b5
João Périco <jeperico@terra.com.br>
2023-10-19 12:24:10 -0300

    Image

--- client/src/layout/Navbar.jsx
+++ client/src/layout/Navbar.jsx
@@ -1,4 +1,5 @@
 import './Navbar.css'
+import logo from "../assets/imgs/logo.png"
 
 function Navbar(){
 
@@ -6,22 +7,26 @@ function Navbar(){
         <div className="container">
             <nav className="navbar navbar-expand-md fixed-top" id="nav-container">
                 <div>
-                    <button className="navbar-toggler ms-3">
+                    <button className="navbar-toggler ms-3" type="button" data-toggle="collapse" data-target="#navbar-links"
+                    aria-controls="navbar-links" aria-expanded="false" aria-label="Toggle navigation">
                         <span id="hamburguer">
-                            <i class="fa-solid fa-bars"></i>
+                            <i className="fa-solid fa-bars"></i>
                         </span>
                     </button>
-                    <img alt="Logo iFood" className="navbar-brand ms-md-4 ms-3" />
+                    <img src={logo} alt="Logo iFood" className="navbar-brand ms-md-4 ms-3" />
                 </div>
                 <div className="collapse navbar-collapse">
                     <ul className="navbar-nav mb-2">
                         <aside>asd</aside>z
                     </ul>
                 </div>
-                <div id="loginIcon">
-                    <img alt="IMG logo" />
+                
+                {/* 
+                <div className='' id="loginIcon">
+                    <img alt="IMG login" />
                     <p>Logo</p>
                 </div>
+                */}
             </nav>
         </div>
     )
